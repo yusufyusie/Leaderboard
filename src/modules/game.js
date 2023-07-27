98f2035d16575aea9498f2b382acc30df4a73efb
yusufyusie <yesuffenta023@gmail.com>
2023-07-27 08:08:02 +0300

    Add styles on css file

--- src/modules/game.js
+++ src/modules/game.js
@@ -1,16 +1,27 @@
+const isgameId = () => localStorage.getItem('gameId');
+
 const createGame = async () => {
-  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
-    method: 'POST',
-    body: JSON.stringify({
-      name: 'Heroes Game',
-    }),
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-  });
-  const gameId = await response.json();
-  return gameId;
+  if (!isgameId()) {
+    await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+      method: 'POST',
+      body: JSON.stringify({
+        name: 'Heroes Game',
+      }),
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+
+      .then(async (response) => {
+        const data = await response.json();
+        const arr = data.result.split(' ');
+        localStorage.setItem('gameId', arr[3]);
+      })
+      .catch((Error) => {
+        throw new Error('Something went wrong!');
+      });
+  }
 };
 
 export default createGame;
